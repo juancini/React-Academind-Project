f06b06e49c00b883a2d6eb0cff82a01d9d7981c6
Juan Mancini <jmancini@atentus.com>
2022-08-25 12:23:45 -0300

    cloned code from academind

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import ExpenseList from "./components/ExpenseList";
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
-    { title: "Ropa", amount: 19800, date: new Date(2022, 6, 28) },
-    { title: "Super", amount: 2500, date: new Date(2021, 6, 29) },
-    { title: "Transporte", amount: 400, date: new Date(2021, 6, 30) },
-    { title: "Caramelos", amount: 500, date: new Date(2021, 6, 28) },
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseList expenses={expenses}/>
+      <Expenses items={expenses} />
     </div>
   );
 }
